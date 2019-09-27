cc308594e5a0e8812e1dc4c3c9010e3509f371b3
Ayush Singh Gaur <ayusin27@in.ibm.com>
2019-09-27 21:03:27 +0530

    =init commit

---
+++ Readme.txt
@@ -0,0 +1,27 @@
+do npm install
+
+to run use:
+npm run dev
+
+
+WORKING 
+in postman-
+
+POST : create user or register user
+
+localhost:3000/users
+
+provide 
+name , email , password in JSON format
+
+GET : login user
+
+localhost:3000/users/login
+
+provide valid
+email and password
+
+POST : logout and logoutAll(for logout from all devices)
+
+localhost:3000/users/logout and 
+localhost:3000/users/logoutAll
--- src/models/user.js
+++ src/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const Task = require('./task')
+
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -33,22 +33,17 @@ const userSchema = new mongoose.Schema({
             }
         }
     },
+   
     tokens: [{
         token: {
             type: String,
             required: true
         }
     }],
-  },  {
-       timestamps : true 
-    
+
 })
 
-userSchema.virtual('tasks', {
-    ref: 'Task',
-    localField: '_id',
-    foreignField: 'owner'
-})
+
 
 userSchema.methods.toJSON = function () {
     const user = this
@@ -96,12 +91,6 @@ userSchema.pre('save', async function (next) {
     next()
 })
 
-// Delete user tasks when user is removed
-userSchema.pre('remove', async function (next) {
-    const user = this
-    await Task.deleteMany({ owner: user._id })
-    next()
-})
 
 const User = mongoose.model('User', userSchema)
 
